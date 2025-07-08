9d63639374f6a89d968cea727cd6a224a573b13a
Geometallurgy <geomet.jl@gmail.com>
2025-07-07 22:44:01 -0300

    Add Mih function implementation in Pluto notebook

---
+++ notebooks/Function_Mih.jl
@@ -0,0 +1,61 @@
+### A Pluto.jl notebook ###
+# v0.20.13
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bde7c0ce-5b9b-11f0-38d1-fb149511671b
+
+
+# ╔═╡ bf601e97-a286-4df4-9555-1562e90bb7d7
+
+
+# ╔═╡ 58d90ba0-ff67-4296-89b1-178140b34f4e
+
+
+# ╔═╡ 86669c28-63a3-47b2-8f52-dff2840ad398
+
+
+# ╔═╡ 5fd741eb-faec-422d-a9ad-bb2856702d5f
+
+
+# ╔═╡ 3671cbae-08b0-433b-a852-b2e98c5a0aae
+
+
+# ╔═╡ d46b6420-a907-406e-9710-cfad9ec65d82
+
+
+# ╔═╡ b104fa95-711a-4dc8-9c41-1fc5f6bf161b
+
+
+# ╔═╡ 7f24ace4-f3b7-4f87-8ce8-348e35a91755
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.9"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═bde7c0ce-5b9b-11f0-38d1-fb149511671b
+# ╠═bf601e97-a286-4df4-9555-1562e90bb7d7
+# ╠═58d90ba0-ff67-4296-89b1-178140b34f4e
+# ╠═86669c28-63a3-47b2-8f52-dff2840ad398
+# ╠═5fd741eb-faec-422d-a9ad-bb2856702d5f
+# ╠═3671cbae-08b0-433b-a852-b2e98c5a0aae
+# ╠═d46b6420-a907-406e-9710-cfad9ec65d82
+# ╠═b104fa95-711a-4dc8-9c41-1fc5f6bf161b
+# ╠═7f24ace4-f3b7-4f87-8ce8-348e35a91755
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
