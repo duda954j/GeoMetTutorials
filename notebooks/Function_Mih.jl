e68ca707e7c9471bbc0160245bcc1460fb629581
Geometallurgy <geomet.jl@gmail.com>
2025-07-17 17:46:23 -0300

    Update Function_Mih.jl
     Pluto notebook to test calculate_mih function

--- notebooks/Function_Mih.jl
+++ notebooks/Function_Mih.jl
@@ -1,61 +1,28 @@
 ### A Pluto.jl notebook ###
 # v0.20.13
 
-using Markdown
-using InteractiveUtils
+using GeoMet
 
-# ╔═╡ bde7c0ce-5b9b-11f0-38d1-fb149511671b
+# ╔═╡ 1
+"""
+# Test of function calculate_mih
 
-
-# ╔═╡ bf601e97-a286-4df4-9555-1562e90bb7d7
-
-
-# ╔═╡ 58d90ba0-ff67-4296-89b1-178140b34f4e
-
-
-# ╔═╡ 86669c28-63a3-47b2-8f52-dff2840ad398
-
-
-# ╔═╡ 5fd741eb-faec-422d-a9ad-bb2856702d5f
-
-
-# ╔═╡ 3671cbae-08b0-433b-a852-b2e98c5a0aae
-
-
-# ╔═╡ d46b6420-a907-406e-9710-cfad9ec65d82
-
-
-# ╔═╡ b104fa95-711a-4dc8-9c41-1fc5f6bf161b
-
-
-# ╔═╡ 7f24ace4-f3b7-4f87-8ce8-348e35a91755
-
-
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
+This notebook tests the `calculate_mih` function from the GeoMet package.
 """
 
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
+# ╔═╡ 2
+# Show the function to confirm it's loaded
+calculate_mih
 
-julia_version = "1.10.9"
-manifest_format = "2.0"
-project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+# ╔═╡ 3
+# Define example parameters
+A = 40.0
+b = 0.5
 
-[deps]
-"""
+# ╔═╡ 4
+# Calculate Mih value with example parameters
+mih_value = calculate_mih(A, b)
 
-# ╔═╡ Cell order:
-# ╠═bde7c0ce-5b9b-11f0-38d1-fb149511671b
-# ╠═bf601e97-a286-4df4-9555-1562e90bb7d7
-# ╠═58d90ba0-ff67-4296-89b1-178140b34f4e
-# ╠═86669c28-63a3-47b2-8f52-dff2840ad398
-# ╠═5fd741eb-faec-422d-a9ad-bb2856702d5f
-# ╠═3671cbae-08b0-433b-a852-b2e98c5a0aae
-# ╠═d46b6420-a907-406e-9710-cfad9ec65d82
-# ╠═b104fa95-711a-4dc8-9c41-1fc5f6bf161b
-# ╠═7f24ace4-f3b7-4f87-8ce8-348e35a91755
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
+# ╔═╡ 5
+# Show the result
+mih_value
